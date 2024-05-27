536cc7d25fee66389d9815ea39495a11338abec6
Kremynt <larbijephtah600@gmail.com>
2024-05-27 17:06:24 +0000

    almost done

--- game_stats.py
+++ game_stats.py
@@ -6,6 +6,9 @@ class GameStats:
         self.settings = ai_game.settings
         self.reset_stats()
         
+        #Start Alien invasion in active state
+        self.game_active = False
+        
         
     def reset_stats(self):
         """initialize statistics that change during game"""
